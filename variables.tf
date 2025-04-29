92aa99b30911c35d9ffd2fcd71f26c3e23ceb1eb
Tommy Cho <tommycho@dhcp-206-87-113-161.ubcsecure.wireless.ubc.ca>
2025-04-29 13:27:01 -0700

    fixing variables and outputs

--- variables.tf
+++ variables.tf
@@ -25,87 +25,6 @@ variable "random_instance_name" {
   default     = false
 }
 
-// required
-variable "database_version" {
-  description = "The database version to use"
-  type        = string
-}
-
-variable "tier" {
-  description = "The tier for the master instance."
-  type        = string
-  default     = "db-f1-micro"
-}
-
-variable "edition" {
-  description = "The edition of the master instance"
-  type        = string
-  default     = null
-}
-
-variable "zone" {
-  type        = string
-  description = "The zone for the master instance, it should be something like: `us-central1-a`, `us-east1-c`."
-}
-
-variable "secondary_zone" {
-  type        = string
-  description = "The secondary_zone for the master instance, it should not include what's in the zone variable: `us-central1-b`, `us-east1-c`."
-}
-variable "activation_policy" {
-  description = "The activation policy for the master instance.Can be either `ALWAYS`, `NEVER` or `ON_DEMAND`."
-  type        = string
-  default     = "ALWAYS"
-}
-
-variable "availability_type" {
-  description = "The availability type for the master instance.This is only used to set up high availability for the PostgreSQL instance. Can be either `ZONAL` or `REGIONAL`."
-  type        = string
-  default     = "ZONAL"
-}
-
-variable "disk_autoresize" {
-  description = "Configuration to increase storage size."
-  type        = bool
-  default     = true
-}
-
-variable "disk_size" {
-  description = "The disk size for the master instance."
-  type        = number
-  default     = 10
-}
-
-variable "disk_type" {
-  description = "The disk type for the master instance."
-  type        = string
-  default     = "PD_SSD"
-}
-
-variable "pricing_plan" {
-  description = "The pricing plan for the master instance."
-  type        = string
-  default     = "PER_USE"
-}
-
-variable "maintenance_window_day" {
-  description = "The day of week (1-7) for the master instance maintenance."
-  type        = number
-  default     = 1
-}
-
-variable "maintenance_window_hour" {
-  description = "The hour of day (0-23) maintenance window for the master instance maintenance."
-  type        = number
-  default     = 23
-}
-
-variable "maintenance_window_update_track" {
-  description = "The update track of maintenance window for the master instance maintenance.Can be either `canary` or `stable`."
-  type        = string
-  default     = "canary"
-}
-
 variable "database_flags" {
   description = "The database flags for the master instance. See [more details](https://cloud.google.com/sql/docs/postgres/flags)"
   type = list(object({
@@ -143,46 +62,6 @@ variable "backup_configuration" {
   }
 }
 
-variable "insights_config" {
-  description = <<EOT
-  The insights_config settings for the database.
-  {
-    query_string_length     = number
-    record_application_tags = bool
-    record_client_address   = bool
-  }
-  EOT
-  type = object({
-    query_insights_enabled  = bool
-    query_string_length     = number
-    record_application_tags = bool
-    record_client_address   = bool
-  })
-  default = {
-    query_insights_enabled  = true
-    query_string_length     = 1024
-    record_application_tags = false
-    record_client_address   = false
-  }
-}
-
-variable "ip_configuration" {
-  description = "The ip configuration for the master instances."
-  type = object({
-    authorized_networks = list(map(string))
-    ipv4_enabled        = bool
-    private_network     = string
-    ssl_mode            = string
-  })
-  default = {
-    authorized_networks = []
-    ipv4_enabled        = true
-    private_network     = null
-    ssl_mode            = null
-  }
-}
-
-// Read Replicas
 variable "read_replicas" {
   description = <<EOT
   List of read replicas to create
@@ -222,18 +101,6 @@ variable "db_name" {
   default     = "default"
 }
 
-variable "db_charset" {
-  description = "The charset for the default database"
-  type        = string
-  default     = ""
-}
-
-variable "db_collation" {
-  description = "The collation for the default database. Example: 'en_US.UTF8'"
-  type        = string
-  default     = ""
-}
-
 variable "additional_databases" {
   description = "A list of databases to be created in your cluster"
   type = list(object({
@@ -266,28 +133,10 @@ variable "additional_users" {
   default = []
 }
 
-variable "iam_user_emails" {
-  description = "A list of IAM users to be created in your cluster"
-  type        = list(string)
-  default     = []
-}
-
-variable "create_timeout" {
-  description = "The optional timout that is applied to limit long database creates."
-  type        = string
-  default     = "15m"
-}
-
-variable "update_timeout" {
-  description = "The optional timout that is applied to limit long database updates."
-  type        = string
-  default     = "15m"
-}
-
-variable "delete_timeout" {
-  description = "The optional timout that is applied to limit long database deletes."
-  type        = string
-  default     = "15m"
+variable "cpu_count" {
+  description = "The number of CPUs for the primary instance"
+  type        = number
+  default     = 2
 }
 
 variable "encryption_key_name" {
@@ -296,44 +145,14 @@ variable "encryption_key_name" {
   default     = null
 }
 
-variable "module_depends_on" {
-  description = "List of modules or resources this module depends on."
-  type        = list(any)
-  default     = []
-}
-
-variable "deletion_protection" {
-  description = "Used to block Terraform from deleting a SQL Instance."
-  type        = bool
-  default     = true
-}
-
-variable "deletion_protection_enabled" {
-  description = "Used to block Terraform from deleting a SQL Instance."
-  type        = bool
-  default     = true
-}
-
-variable "read_replica_deletion_protection" {
-  description = "Used to block Terraform from deleting replica SQL Instances."
-  type        = bool
-  default     = false
-}
-
-variable "enable_default_db" {
-  description = "Enable or disable the creation of the default database"
-  type        = bool
-  default     = true
+variable "availability_type" {
+  description = "The availability type for the master instance.This is only used to set up high availability for the PostgreSQL instance. Can be either `ZONAL` or `REGIONAL`."
+  type        = string
+  default     = "ZONAL"
 }
 
 variable "enable_default_user" {
   description = "Enable or disable the creation of the default user"
   type        = bool
   default     = true
-}
-
-variable "cpu_count" {
-  description = "The number of CPUs for the primary instance"
-  type        = number
-  default     = 2
 }
