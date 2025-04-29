382e65209c7ab0a84eafd42af6ed8689cb868a5e
Tommy Cho <tommycho@dhcp-206-87-113-161.ubcsecure.wireless.ubc.ca>
2025-04-29 12:27:33 -0700

    init files

---
+++ main.tf
@@ -0,0 +1,244 @@
+resource "random_id" "suffix" {
+  count       = var.random_instance_name ? 1 : 0
+  byte_length = 4
+}
+
+locals {
+  cluster_name = var.random_instance_name ? "${var.name}-${random_id.suffix[0].hex}" : var.name
+
+  ip_configuration_enabled = length(keys(var.ip_configuration)) > 0 ? true : false
+
+  ip_configurations = {
+    enabled  = var.ip_configuration
+    disabled = {}
+  }
+
+  databases = { for db in var.additional_databases : db.name => db }
+  users     = { for u in var.additional_users : u.name => u }
+  iam_users = [for iu in var.iam_user_emails : {
+    email       = iu,
+    user_type   = can(regex("serviceAccount:", iu)) ? "CLOUD_IAM_SERVICE_ACCOUNT" : can(regex("group:", iu)) ? "CLOUD_IAM_GROUP" : "CLOUD_IAM_USER",
+    clean_email = regex("^.*:(.*)$", iu)[0]
+
+  }]
+
+  retained_backups = lookup(var.backup_configuration, "retained_backups", null)
+  retention_unit   = lookup(var.backup_configuration, "retention_unit", null)
+}
+
+resource "google_alloydb_cluster" "default" {
+  cluster_id   = local.cluster_name
+  location     = var.region
+  project      = var.project_id
+  network      = var.network
+
+  initial_user {
+    user     = var.user_name
+    password = var.user_password == "" ? random_id.user_password.hex : var.user_password
+  }
+}
+
+resource "random_id" "user_password" {
+  byte_length = 8
+}
+
+resource "google_alloydb_instance" "primary" {
+  cluster       = google_alloydb_cluster.default.name
+  instance_id   = "${local.cluster_name}-primary"
+  instance_type = "PRIMARY"
+  
+  machine_config {
+    cpu_count = var.cpu_count
+  }
+
+  depends_on = [google_alloydb_cluster.default]
+}
+
+resource "google_sql_database_instance" "default" {
+  provider            = google-beta
+  project             = var.project_id
+  name                = local.cluster_name
+  database_version    = var.database_version
+  region              = var.region
+  encryption_key_name = var.encryption_key_name
+  deletion_protection = var.deletion_protection
+
+  settings {
+    tier              = var.tier
+    edition           = var.edition
+    activation_policy = var.activation_policy
+    availability_type = var.availability_type
+    deletion_protection_enabled = var.deletion_protection_enabled
+
+    dynamic "backup_configuration" {
+      for_each = [var.backup_configuration]
+      content {
+        binary_log_enabled             = false
+        enabled                        = lookup(backup_configuration.value, "enabled", null)
+        start_time                     = lookup(backup_configuration.value, "start_time", null)
+        location                       = lookup(backup_configuration.value, "location", null)
+        point_in_time_recovery_enabled = lookup(backup_configuration.value, "point_in_time_recovery_enabled", false)
+        transaction_log_retention_days = lookup(backup_configuration.value, "transaction_log_retention_days", null)
+
+        dynamic "backup_retention_settings" {
+          for_each = local.retained_backups != null || local.retention_unit != null ? [var.backup_configuration] : []
+          content {
+            retained_backups = local.retained_backups
+            retention_unit   = local.retention_unit
+          }
+        }
+      }
+    }
+    dynamic "ip_configuration" {
+      for_each = [local.ip_configurations[local.ip_configuration_enabled ? "enabled" : "disabled"]]
+      content {
+        ipv4_enabled    = lookup(ip_configuration.value, "ipv4_enabled", null)
+        private_network = lookup(ip_configuration.value, "private_network", null)
+        ssl_mode        = lookup(ip_configuration.value, "ssl_mode", null)
+
+        dynamic "authorized_networks" {
+          for_each = lookup(ip_configuration.value, "authorized_networks", [])
+          content {
+            expiration_time = lookup(authorized_networks.value, "expiration_time", null)
+            name            = lookup(authorized_networks.value, "name", null)
+            value           = lookup(authorized_networks.value, "value", null)
+          }
+        }
+      }
+    }
+
+    # Query Insights is free, enable it by default
+    insights_config {
+      query_insights_enabled  = true
+      query_string_length     = lookup(var.insights_config, "query_string_length", 1024)
+      record_application_tags = lookup(var.insights_config, "record_application_tags", true)
+      record_client_address   = lookup(var.insights_config, "record_client_address", true)
+    }
+
+    disk_autoresize = var.disk_autoresize
+    disk_size       = var.disk_size
+    disk_type       = var.disk_type
+    pricing_plan    = var.pricing_plan
+    dynamic "database_flags" {
+      for_each = var.database_flags
+      content {
+        name  = lookup(database_flags.value, "name", null)
+        value = lookup(database_flags.value, "value", null)
+      }
+    }
+
+    user_labels = var.user_labels
+
+    location_preference {
+      zone           = var.zone
+      secondary_zone = var.secondary_zone
+    }
+
+    maintenance_window {
+      day          = var.maintenance_window_day
+      hour         = var.maintenance_window_hour
+      update_track = var.maintenance_window_update_track
+    }
+  }
+
+  lifecycle {
+    ignore_changes = [
+      settings[0].disk_size
+    ]
+  }
+
+  timeouts {
+    create = var.create_timeout
+    update = var.update_timeout
+    delete = var.delete_timeout
+  }
+
+  depends_on = [null_resource.module_depends_on]
+}
+
+resource "google_sql_database" "default" {
+  count      = var.enable_default_db ? 1 : 0
+  name       = var.db_name
+  project    = var.project_id
+  instance   = google_sql_database_instance.default.name
+  charset    = var.db_charset
+  collation  = var.db_collation
+  depends_on = [null_resource.module_depends_on, google_sql_database_instance.default]
+}
+
+resource "google_sql_database" "additional_databases" {
+  for_each   = local.databases
+  project    = var.project_id
+  name       = each.value.name
+  charset    = lookup(each.value, "charset", null)
+  collation  = lookup(each.value, "collation", null)
+  instance   = google_sql_database_instance.default.name
+  depends_on = [null_resource.module_depends_on, google_sql_database_instance.default]
+}
+
+resource "random_id" "user-password" {
+  keepers = {
+    name = google_sql_database_instance.default.name
+  }
+
+  byte_length = 8
+  depends_on  = [null_resource.module_depends_on, google_sql_database_instance.default]
+}
+
+resource "google_sql_user" "default" {
+  count      = var.enable_default_user ? 1 : 0
+  name       = var.user_name
+  project    = var.project_id
+  instance   = google_sql_database_instance.default.name
+  password   = var.user_password == "" ? random_id.user-password.hex : var.user_password
+  depends_on = [null_resource.module_depends_on, google_sql_database_instance.default]
+
+  lifecycle {
+    ignore_changes = [password]
+  }
+}
+
+resource "google_sql_user" "additional_users" {
+  for_each   = local.users
+  project    = var.project_id
+  name       = each.value.name
+  password   = coalesce(each.value["password"], random_id.user-password.hex)
+  instance   = google_sql_database_instance.default.name
+  depends_on = [null_resource.module_depends_on, google_sql_database_instance.default]
+  lifecycle {
+    ignore_changes = [password]
+  }
+}
+
+resource "google_project_iam_member" "iam_binding" {
+  for_each = {
+    for iu in local.iam_users :
+    "${iu.email} ${iu.user_type}" => iu
+  }
+  project = var.project_id
+  role    = "roles/cloudsql.instanceUser"
+  member  = each.value.email
+}
+
+resource "google_sql_user" "iam_account" {
+  for_each = {
+    for iu in local.iam_users :
+    "${iu.email} ${iu.user_type}" => iu
+  }
+  project = var.project_id
+  name    = each.value.user_type == "CLOUD_IAM_SERVICE_ACCOUNT" ? trimsuffix(each.value.clean_email, ".gserviceaccount.com") : each.value.clean_email
+
+  instance = google_sql_database_instance.default.name
+  type     = each.value.user_type
+
+  depends_on = [
+    null_resource.module_depends_on,
+    google_project_iam_member.iam_binding,
+  ]
+}
+
+resource "null_resource" "module_depends_on" {
+  triggers = {
+    value = length(var.module_depends_on)
+  }
+}
